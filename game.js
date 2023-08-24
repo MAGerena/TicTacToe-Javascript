9e2848dc5eb719084e1efca47f4b081ec4c0a518
Miguel Ángel Gerena <miguegerena@gmail.com>
2023-08-24 19:23:31 +0200

    v 0.1

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 const board = document.querySelector("#board");
-const tiles =  ["","","",
+const infoPanel = document.querySelector("#info")
+const startTiles =  ["","","",
                 "","","",
                 "","",""];
 let turn = 0;
@@ -7,7 +8,7 @@ let turn = 0;
 initBoard();
 
 function initBoard() {
-    tiles.forEach((Element, index) => {
+    startTiles.forEach((Element, index) => {
         const tile = document.createElement("div");
         tile.classList.add("tile");
         board.append(tile);
@@ -27,4 +28,39 @@ function insertToken(e) {
         turn = 1;
     }
     e.target.removeEventListener("click", insertToken);
+    checkWinner();
+}
+
+function checkWinner() {
+    const tiles = document.querySelectorAll(".tile");
+    const winningCombinations = [
+        [0,1,2], [3,4,5], [6,7,8],
+        [0,3,6], [1,4,7], [2,5,8],
+        [0,4,8], [2,4,6]
+    ]
+
+    winningCombinations.forEach(array => {
+        const circleWins = array.every(tile => 
+            tiles[tile].innerHTML === "O"
+        )
+        if(circleWins) {
+            infoPanel.innerHTML = "circles wins";
+            for (let index = 0; index < tiles.length; index++) {
+                tiles[index].removeEventListener("click", insertToken);
+            }
+        }
+    })
+
+    winningCombinations.forEach(array => {
+        const crossWins = array.every(tile => 
+            tiles[tile].innerHTML === "X"
+        )
+        if(crossWins) {
+            infoPanel.innerHTML = "Cross wins";
+            for (let index = 0; index < tiles.length; index++) {
+                tiles[index].removeEventListener("click", insertToken);
+            }
+        }
+    })
+
 }
